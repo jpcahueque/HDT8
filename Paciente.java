dd15e8ebe886ca7858375529f42187d8cea394e4
enmalopezbran <enmalopezbran@gmail.com>
2016-10-23 17:32:36 -0600

    Update Paciente.java

--- Paciente.java
+++ Paciente.java
@@ -1,10 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package hdt8;
 
+package hdt8;
 
 
 
@@ -72,4 +67,7 @@ public class Paciente implements Comparable<Paciente>{
     }
    
     
+
+    
 }
+
